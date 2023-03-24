669cdaecd68093c3564c518108a47ec87151f593
tehnickge <71207932+tehnickge@users.noreply.github.com>
2023-03-24 08:31:29 +0300

    add bouble sort

--- asdsaas/second_try.h
+++ asdsaas/second_try.h
@@ -6,9 +6,4 @@ namespace secnond_try
 	vector<vector<int>> generateVectorWithData(int k = 25);
 	string surchData(vector<vector<int>> vec, int value, int surchValue);
 	vector<vector<int>> boubleSort(vector<vector<int>> vec, int posCount);
-	vector<vector<int>> boubleSort(vector<vector<int>> vec, int posCount);
-	vector<vector<int>> boubleSort(vector<vector<int>> vec, int posCount);
-	vector<vector<int>> boubleSort(vector<vector<int>> vec, int posCount);
-	vector<vector<int>> boubleSort(vector<vector<int>> vec, int posCount);
-	vector<vector<int>> boubleSort(vector<vector<int>> vec, int posCount);
 }
